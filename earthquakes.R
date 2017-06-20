82554ae4c6eb59dd41d672ebf082a0e7d283b2e7
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-20 21:19:04 +0530

    Density plot of Magnitude colored by itd type

--- earthquakes.R
+++ earthquakes.R
@@ -42,20 +42,45 @@ yeardf<-earthq %>%
     filter(Type=="Earthquake",complete.cases(Year)==T) %>%
   #complete.cases==T to excllude NA values
     group_by(Year) %>%
-    summarise(Count = n() , Mean_Magntidue = mean(Magnitude) )
+    summarise(Count = n() , Mean_Magnitude = mean(Magnitude) )
+
+#Converting Year column to numeric Type for plotting
+yeardf$Year<-as.numeric(yeardf$Year)
 
 
-
-ggplot(yeardf , aes(as.numeric(Year) , Count)) + 
-   geom_line(color="red") + 
-    geom_point(color="red") + 
-    scale_x_continuous(limits = c(1965,2016),breaks = seq(1965,2016,10))
+ggplot(yeardf , aes(Year , Count)) + 
+   geom_line(color="purple") + 
+    geom_point(color="purple") + 
+    scale_x_continuous(limits = c(1965,2016),breaks= seq(1965,2016,10))  + 
+  labs(title="Time Series Plot of Year vs Number of Earthquakes",
+       x = "Year " , y ="Count")
   
 #Highest number of Earthquakes in Year 2011  
   
 
 
+#Time Series of Mean Magnitudes vs Year
+
+ggplot(aes(x = Year , y = Mean_Magnitude),data = yeardf) + 
+  geom_line(color="blue")  + geom_point(color="blue") + 
+  scale_x_continuous(limits = c(1965,2016),breaks = seq(1965,2016,10))  + 
+  labs(title="Time Series Plot of Year vs Mean of Magnitudes for that Year",
+       x = "Year " , y ="Mean Magnitudes")
+#Mean Magnitues of Earthquakes higher for years 1965 to 1970 after then dropped
+  
 
 
 
+#Distribution of Magnitudes Type
+table(Magnitude_Type = na.omit(earthq$Magnitude.Type))
+
+ggplot(aes(Magnitude.Type),data = earthq) + 
+  geom_bar(aes(fill=Magnitude.Type,color=Magnitude.Type,alpha = 0.7)) + 
+  labs(title="Distribution of Magnitude Types", x = "Type of magnitude" , y="Count")
+#Highest for type MW
+
+#Density Plot of Magnitude colored by its type- Smoothed version of Histograms
+ggplot(aes(Magnitude),data = earthq) + 
+  geom_density(aes(fill=Magnitude.Type,color=Magnitude.Type,alpha=0.001)) + 
+  labs(title="Density Plot of Magnitude ")
 
